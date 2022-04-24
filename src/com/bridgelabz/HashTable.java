675122125addb1990d487f9149e11a7480ed4f38
Rajkawale7 <Rajkawale712@gmail.com>
2022-04-24 23:52:16 +0530

    Added: Program to find Frequency of words in large Paragraph

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -1,9 +1,7 @@
 package com.bridgelabz;
 
 public class HashTable {
-
-
-    static final int size = 5;
+	static final int size = 5;
 
     public static int hashFunction(String str) {
         return str.hashCode() % size;
@@ -17,32 +15,25 @@ public class HashTable {
         int j = 0;
         int count = 0;
 
-
         for (String str : sentenceArray) {
             System.out.println(str + "----" + hashFunction(str));
-        //    System.out.println(sentenceArray);
+            //    System.out.println(sentenceArray);
 
         }
-        System.out.println(sentenceArray.length);
-        while (i != (sentenceArray.length )) {
-            while (j != (sentenceArray.length )) {
-                if (sentenceArray[i].equals(sentenceArray[j])) {
-                    count = +1;
-                }
-
-                j++;
-            }
-            System.out.println("Frequency: of " + sentenceArray[i] + ": " + count);
-            count = 0;
-            i++;
-            j=0;
-        }
-
     }
+    public static void paragraph() {
 
+        String paragraph = "Paranoids are paranoid because they are Paranoid but because they keep putting" +
+                " themselves deliberately into Paranoid avoidable situations";
+        String[] paragraphArray = paragraph.split(" ");
+        for (String str : paragraphArray) {
+            System.out.println(str + "----" + hashFunction(str));
+        }
+    }
     public static void main(String[] args) {
         HashTable.sentence();
-
+        System.out.println("");
+        HashTable.paragraph();
     }
-}
 
+}
